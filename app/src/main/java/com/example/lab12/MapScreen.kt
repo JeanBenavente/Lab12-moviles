9fe704770e4db336ffc2974549e88d45927d724f
Jean Benavente <jean.benavente@tecsup.edu.pe>
2025-06-09 12:19:49 -0500

    funcion de marcador

--- app/src/main/java/com/example/lab12/MapScreen.kt
+++ app/src/main/java/com/example/lab12/MapScreen.kt
@@ -1,37 +1,44 @@
 package com.example.lab12
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.CameraPosition
 import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.rememberCameraPositionState
 import com.google.maps.android.compose.rememberMarkerState
-import com.google.android.gms.maps.model.LatLng
-import com.google.maps.android.compose.Marker
-
 
 @Composable
 fun MapScreen() {
-    val ArequipaLocation = LatLng(-16.4040102, -71.559611) // Arequipa, Perú
+    val context = LocalContext.current
+    val arequipa = LatLng(-16.4040102, -71.559611)
     val cameraPositionState = rememberCameraPositionState {
-        position = com.google.android.gms.maps.model.CameraPosition.fromLatLngZoom(ArequipaLocation, 12f)
+        position = CameraPosition.fromLatLngZoom(arequipa, 12f)
     }
 
-
     Box(modifier = Modifier.fillMaxSize()) {
-        // Añadir GoogleMap al layout
         GoogleMap(
             modifier = Modifier.fillMaxSize(),
-            cameraPositionState = cameraPositionState
+            cameraPositionState = cameraPositionState,
         ) {
-            // Añadir marcador en Denver, Colorado
+            val context = LocalContext.current
+            val originalBitmap = BitmapFactory.decodeResource(context.resources, R.drawable.mountain)
+            val resizedBitmap = Bitmap.createScaledBitmap(originalBitmap, 100, 100, false)
+            val customIcon = BitmapDescriptorFactory.fromBitmap(resizedBitmap)
+
             Marker(
-                state = rememberMarkerState(position = ArequipaLocation),
+                state = rememberMarkerState(position = arequipa),
+                icon = customIcon,
                 title = "Arequipa, Perú"
             )
         }
     }
 }
+
