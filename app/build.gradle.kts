c0b2d8c7da612a1a74eaf1aacd69f4765487bfa1
Jean Benavente <jean.benavente@tecsup.edu.pe>
2025-06-09 11:38:38 -0500

    Dependencias añadidas

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,17 @@ android {
 
 dependencies {
 
+    val mapsComposeVersion = "4.4.1"
+
+    // Google Maps Compose
+    implementation("com.google.maps.android:maps-compose:$mapsComposeVersion")
+
+    // Utilidades de Google Maps para Jetpack Compose
+    implementation("com.google.maps.android:maps-compose-utils:$mapsComposeVersion")
+
+    // Widgets de Google Maps Compose
+    implementation("com.google.maps.android:maps-compose-widgets:$mapsComposeVersion")
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -56,4 +67,5 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
 }
